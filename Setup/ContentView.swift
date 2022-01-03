cc6366e40f4200aa0248e99cd1c30b11b52ca46d
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-01-03 22:19:54 +0800

    Initial Commit

---
+++ Setup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Setup
+//
+//  Created by Jia Chen Yee on 3/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Setup/SetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetupApp.swift
+//  Setup
+//
+//  Created by Jia Chen Yee on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
